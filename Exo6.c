ca7d20a4b860a244ca289e77b2bff1a2a745686b
louisdeclerck <49341698+louisdeclerck@users.noreply.github.com>
2019-04-07 15:10:03 +0200

    Add files via upload

---
+++ Exo6.c
@@ -0,0 +1,141 @@
+// pour compiler ce fichier executer gcc Exo6.c O9 -o Exo6 -std=c99
+
+// pour executer faire : ./Exo6 degreMon_Fichier.txt
+
+
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <strings.h>
+#include <time.h>
+
+
+
+#define nbarete 100000000 //a adapter au fichier
+
+
+typedef struct {
+
+	unsigned long s;
+	unsigned long t;
+	
+} arete;
+
+
+
+typedef struct {
+
+	unsigned long n;
+	unsigned long e;
+	arete *edges;
+	unsigned long *dout;
+
+} listarete;
+
+
+
+inline unsigned long max3(unsigned long a,unsigned long b,unsigned long c){
+
+	a=(a>b) ? a : b;
+	return (a>c) ? a : c;
+
+}
+
+
+listarete* lirelistarete(char* input){
+
+	unsigned long k=nbarete;
+	listarete *g=malloc(sizeof(listarete));
+	FILE *file;
+
+
+	g->n=0;
+	g->e=0;
+	
+	file=fopen(input,"r");
+	g->edges=malloc(k*sizeof(arete));
+
+	while (fscanf(file,"%d %d", &(g->edges[g->e].s), &(g->edges[g->e].t))==2) {
+		g->n=max3(g->n,g->edges[g->e].s,g->edges[g->e].t);
+
+		if (g->e++==k) {
+
+			k+=nbarete;
+			g->edges=realloc(g->edges,k*sizeof(arete));
+
+		}
+
+	}
+
+	fclose(file);
+	
+	g->n++;
+
+	g->edges=realloc(g->edges,g->e*sizeof(arete));
+
+
+/*
+	g->dout=calloc(g->n,sizeof(unsigned long));
+	
+	int c=0;
+	
+	for (int i=0; i< g->n; i++)
+	{
+		for (int j=0; j< g->e; j++)
+			{
+
+				if (g->edges[j].s == i)
+				{
+					c=c+1;
+				}
+			}
+			g->dout[i]=c;
+			c=1;
+		}
+	*/
+
+	return g;
+
+}
+
+
+
+
+int main(int argc,char** argv){
+	listarete* g;
+	
+	printf("Lecture à partir du fichier %s\n",argv[1]);
+	
+
+	g=lirelistarete(argv[1]);
+		
+	FILE* fichier = NULL;
+	
+	fichier= fopen("ddamazon.txt","w+");
+
+	
+	for (int i=0; i< g->n; i++)
+	{
+		int c=0;
+		for (int j=0; j< g->n; j++)
+			{
+				if (g->edges[j].t == i)
+				{
+					c=c+1;
+				}
+			}
+		fprintf(fichier,"%d %d\n",i,c);
+		printf("%d\n",i);
+		
+
+	}
+
+	
+	fclose(fichier);
+	
+
+
+	return 0;
+}
